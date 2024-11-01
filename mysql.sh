1fa85a966a16b4ea066afece0f6850e552708e8e
Yaswanthcode <yaswanth096@gmail.com>
2024-11-01 14:18:39 +0530

    expenses automation

--- mysql.sh
+++ mysql.sh
@@ -30,11 +30,21 @@ fi
 dnf install mysql-server -y &>>LOGFILE
 VALIDATE $? "Installation of MySQL"
 
-systemctl enable mysqlld -y &>>LOGFILE
+systemctl enable mysqld -y &>>LOGFILE
 VALIDATE $? "Enabling MySQL "
 
 systemctl start mysqld &>>LOGFILE
 VALIDATE $? "starting MySQL "
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
-VALIDATE $? "Setting up Password "
+
+mysql -h localhost -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    VALIDATE $? "MySQL Root password Setup"
+else
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+fi
+
+#mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
+#VALIDATE $? "Setting up Password "
